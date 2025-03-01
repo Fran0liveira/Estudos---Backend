5ce8e8e0890ebeae9e6fc2056842921e31595197
Francisco Leonardo Leite Oliveira <francisco.leonardo11@outlook.com>
2025-03-01 11:09:07 -0300

    testando ainda

--- src/main/java/com/ksoft/estudos/EstudosApplication.java
+++ src/main/java/com/ksoft/estudos/EstudosApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EstudosApplication {
 
 	public static void main(String[] args) {
+		System.out.println("RODANDO APLICACAO");
 		SpringApplication.run(EstudosApplication.class, args);
 	}
 
